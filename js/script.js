a3de93c9f954e36b2f82fbb97857163c378969e2
daniel-c <dandaman1719@hotmail.com>
2022-01-19 13:41:45 -0700

    Created a function to capture user input and check players input. Added a new global variable for player guesses and validated input within the button event handler.

--- js/script.js
+++ js/script.js
@@ -8,6 +8,8 @@ const message = document.querySelector(".message");
 const playAgainButton = document.querySelector("play-again");
 
 const word = "magnolia";
+const guessedLetters = [];
+
 
 
 const placeholder = function (word) {
@@ -23,7 +25,36 @@ placeholder(word);
 
 guessButton.addEventListener("click", function (e) {
     e.preventDefault();
+    message.innerText = "";
     const guess = letterInput.value;
     console.log(guess);
     letterInput.value = "";
+    const goodGuess = validateInput(guess);
+    if (goodGuess) {
+        makeGuess(guess);
+    }
+    letterInput.value = "";
 });
+
+const validateInput = function (input) {
+    const acceptedLetter = /[a-zA-Z]/;
+    if (input.lenght === 0) {
+        message.innerText = "Please enter a letter.";
+    }   else if (input.length > 1) {
+        message.innerText = "Please enter only one single letter.";
+    }   else if (!input.match(acceptedLetter)) {
+        message.innerText = "Please enter a letter from A to Z.";
+    }   else {
+        return input;
+    }       
+};
+
+const makeGuess = function (guess) {
+    guess = guess.toUpperCase();
+    if (guessedLetters.includes(guess)) {
+        message.innerText = "That letter has been guessed! Please try a different letter!";
+    }   else {
+        guessedLetters.push(guess);
+        console.log(guessedLetters);
+    }
+};
